6b37ea79ed35d9a379e525e6169519936e15ff61
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-28 22:36:54 +0300

    chore(core): clean up code after database updates + new commits topics

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { BasicHttpExceptionFilter } from './filters/basic-http-exception.filter';
 import { ValidationErrorFilter } from './filters/validation-error.filter';
-import { SequelizeMigrations } from './config/migrations/sequelize.migrations';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const seq = new SequelizeMigrations();
-
-  seq
-    .updateUserTable()
-    .then(() => {
-      console.log('Table updated');
-    })
-    .catch((err) => {
-      console.error(err);
-    });
 
   app.useGlobalFilters(
     new BasicHttpExceptionFilter(),
