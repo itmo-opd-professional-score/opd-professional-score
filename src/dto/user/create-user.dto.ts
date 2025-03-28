4e074c5a7a24a59c7ced0a4bc94c685544807a14
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-29 00:21:30 +0300

    feat(user): updated CRUD for users (added gender & age)

--- src/dto/user/create-user.dto.ts
+++ src/dto/user/create-user.dto.ts
@@ -4,6 +4,8 @@ import { GenderEnum } from '../../config/enums/gender.enum';
 export class CreateUserDto {
   username: string;
   email: string;
+  age: Date;
+  gender: GenderEnum;
   role: RolesEnum;
   password: string;
   isBanned: boolean;
