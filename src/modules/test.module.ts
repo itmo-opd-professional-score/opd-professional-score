0e81b48ba597d2f1914d752b1fb87c1a3b5cde5d
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-31 01:19:32 +0300

    chore(core): removed legacy code

--- src/modules/test.module.ts
+++ src/modules/test.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { Test } from '../entities/test.entity';
-import { TestBlock } from '../entities/test-blocks.entity';
-import { TestToTestBlock } from '../entities/test-to-test-block.entity';
 import { TestTypes } from '../entities/test-types.entity';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { TestToUserDashboard } from '../entities/test-to-user-dashboard.entity';
 import { JwtDecoderUtil } from '../utils/jwt-decoder.util';
 import { TestTypesProvider } from '../providers/test-types.provider';
 import { TestTypesController } from '../controllers/test-types.controller';
-import { SectionProvider } from '../providers/section.provider';
 import { UserModule } from './user.module';
 import { SimpleLightTestEntity } from '../entities/simple-light-test.entity';
 import { SimpleLightTestController } from '../controllers/simple-light-test.controller';
@@ -34,10 +29,6 @@ import { TestsController } from '../controllers/tests.controller';
   imports: [
     SequelizeModule.forFeature([
       TestTypes,
-      Test,
-      TestBlock,
-      TestToTestBlock,
-      TestToUserDashboard,
       SimpleLightTestEntity,
       SimpleSoundTestEntity,
       HardLightTestEntity,
@@ -60,7 +51,6 @@ import { TestsController } from '../controllers/tests.controller';
     InvitationTestsProvider,
     TestValidationStrategy,
     TestTypesProvider,
-    SectionProvider,
     SimpleLightTestProvider,
     SimpleSoundTestProvider,
     HardLightTestProvider,
