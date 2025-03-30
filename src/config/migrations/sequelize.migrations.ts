e0b0d325356d70fe6f42da42cf7f565cefd62554
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-30 22:48:14 +0300

    chore(core): removed legacy after fixing

--- src/config/migrations/sequelize.migrations.ts
+++ src/config/migrations/sequelize.migrations.ts
@@ -14,41 +14,6 @@ const sequelize = new Sequelize(
 
 export class SequelizeMigrations {
   public async updateUserTable() {
-    await sequelize.query(
-      `ALTER TABLE "simple-light-test"
-          ALTER COLUMN "averageCallbackTime" TYPE FLOAT;
-      ALTER TABLE "simple-light-test"
-          ALTER COLUMN "dispersion" TYPE FLOAT;
-      ALTER TABLE "simple-light-test"
-          ALTER COLUMN "userId" DROP NOT NULL;
-      `,
-    );
-
-    await sequelize.query(
-      `ALTER TABLE "hard-light-test"
-          ALTER COLUMN "dispersion" TYPE FLOAT;
-      ALTER TABLE "hard-light-test"
-          ALTER COLUMN "averageCallbackTime" TYPE FLOAT;
-      ALTER TABLE "hard-light-test"
-          ALTER COLUMN "userId" DROP NOT NULL;`,
-    );
-
-    await sequelize.query(
-      `ALTER TABLE "simple-sound-test"
-          ALTER COLUMN "dispersion" TYPE FLOAT;
-      ALTER TABLE "simple-sound-test"
-          ALTER COLUMN "averageCallbackTime" TYPE FLOAT;
-      ALTER TABLE "simple-sound-test"
-          ALTER COLUMN "userId" DROP NOT NULL;
-
-      `,
-    );
-
-    await sequelize.query(
-      `ALTER TABLE "addition-test" ALTER COLUMN "dispersion" TYPE FLOAT;
-      ALTER TABLE "addition-test" ALTER COLUMN "averageCallbackTime" TYPE FLOAT;
-      ALTER TABLE "addition-test"
-          ALTER COLUMN "userId" DROP NOT NULL;`,
-    );
+    await sequelize.query(``);
   }
 }
