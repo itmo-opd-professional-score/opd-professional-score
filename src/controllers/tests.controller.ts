a924bd7d8c848995f9cc95c599d1d8b07dcdd729
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-30 23:45:10 +0300

    feat(tests): created method for generation test links

--- src/controllers/tests.controller.ts
+++ src/controllers/tests.controller.ts
@@ -1,16 +1,25 @@
-import { Body, Controller, Inject, Patch } from '@nestjs/common';
+import { Body, Controller, Inject, Patch, Post } from '@nestjs/common';
 import { UpdateTestUserIdStrategy } from '../strategies/update-test-user-id.strategy';
 import { UpdateUserIdsDto } from '../dto/test/update-user-ids.dto';
+import { CreateTestLinkJwtDto } from '../dto/jwt/create-test-link-jwt.dto';
+import { JwtTestLinksGeneratorUtil } from '../utils/jwt-test-links-generator.util';
 
 @Controller('/test')
 export class TestsController {
   constructor(
     @Inject(UpdateTestUserIdStrategy)
     private userIdStrategy: UpdateTestUserIdStrategy,
+    @Inject(JwtTestLinksGeneratorUtil)
+    private jwtLinksGenerator: JwtTestLinksGeneratorUtil,
   ) {}
 
   @Patch('/updateUserIDs')
   public async updateUserIDs(@Body() data: UpdateUserIdsDto) {
     return await this.userIdStrategy.updateUserIDs(data);
+  }
+
+  @Post('/getInvitationTestToken')
+  public async createTestLinkJwt(@Body() data: CreateTestLinkJwtDto) {
+    return await this.jwtLinksGenerator.createToken(data);
   }
 }
--- src/utils/jwt-test-links-generator.util.ts
+++ src/utils/jwt-test-links-generator.util.ts
@@ -2,12 +2,21 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateTestLinkJwtDto } from '../dto/jwt/create-test-link-jwt.dto';
 import { JwtService } from '@nestjs/jwt';
 import { GetTestLinkJwtDto } from '../dto/jwt/get-test-link-jwt.dto';
+import { InvalidEnumSyntaxException } from '../exceptions/validation/invalid-enum-syntax.exception';
+import { TestTypes } from '../entities/test-types.entity';
 
 @Injectable()
 export class JwtTestLinksGeneratorUtil {
   constructor(@Inject(JwtService) private jwtService: JwtService) {}
 
-  public createToken(data: CreateTestLinkJwtDto): string {
+  public async createToken(data: CreateTestLinkJwtDto): Promise<string> {
+    const localTestTypes = await TestTypes.findAll({
+      where: { name: data.testType.trim() },
+    });
+
+    if (localTestTypes.length == 0)
+      throw new InvalidEnumSyntaxException('TestTypes', data.testType);
+
     return this.jwtService.sign(data);
   }
 
