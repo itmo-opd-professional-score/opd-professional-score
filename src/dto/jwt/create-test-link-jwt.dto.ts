21abb0e3d3214d11a3bc55188e2693c77783473e
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-30 13:43:12 +0300

    refactor(core): renamed jwt DTOs

--- src/dto/jwt/create-test-link-jwt.dto.ts
+++ src/dto/jwt/create-test-link-jwt.dto.ts
@@ -1,4 +1,4 @@
-export interface CreateTestLinkDto {
+export interface CreateTestLinkJwtDto {
   testId: number | null;
   testType: string;
 }
--- src/dto/jwt/get-test-link-jwt.dto.ts
+++ src/dto/jwt/get-test-link-jwt.dto.ts
@@ -1,4 +1,4 @@
-export interface GetJwtTestLinkDto {
+export interface GetTestLinkJwtDto {
   testId: number | null;
   testType: string;
   iat: number;
