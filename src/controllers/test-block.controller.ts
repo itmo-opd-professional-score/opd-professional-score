44ae736285296054e363fc987ce039479a8c9be9
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-04-02 12:26:32 +0300

    feat(testBlocks): created logic for testBlocks

---
+++ src/controllers/test-block.controller.ts
@@ -0,0 +1,54 @@
+import { TestBlockProvider } from '../providers/test-block.provider';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateTestBlockDto } from '../dto/test/test-blocks/create-test-block.dto';
+import { AssignUsersToTestBlockDto } from '../dto/test/test-blocks/assign-users-to-test-block.dto';
+
+@Controller('/testBlock')
+export class TestBlockController {
+  constructor(
+    @Inject(TestBlockProvider) private testBlockProvider: TestBlockProvider,
+  ) {}
+
+  @Get('/getAll')
+  public async getAll() {
+    return await this.testBlockProvider.getAll();
+  }
+
+  @Get('/getById/:id')
+  public async getById(@Param('id') id: number) {
+    return await this.testBlockProvider.getById(id);
+  }
+
+  @Get('/getUsersByTBId/:id')
+  public async getAllUsersByToken(@Param('id') id: number) {
+    return await this.testBlockProvider.getAllUsersByTokenId(id);
+  }
+
+  @Get('/getByUserId/:id')
+  public async getByUserId(@Param('id') id: number) {
+    return await this.testBlockProvider.getByUserId(id);
+  }
+
+  @Post('/create')
+  public async create(@Body() data: CreateTestBlockDto) {
+    return await this.testBlockProvider.createTestBlock(data);
+  }
+
+  @Post('/assignUsers')
+  public async assignUsers(@Body() data: AssignUsersToTestBlockDto) {
+    return await this.testBlockProvider.assignTestBlockToUsers(data);
+  }
+
+  @Delete('delete/:id')
+  public async remove(@Param('id') id: number) {
+    return await this.testBlockProvider.delete(id);
+  }
+}
