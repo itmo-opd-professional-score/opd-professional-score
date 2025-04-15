a3a913123855962329b0de33b0eeffc61c514313
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-04-16 02:15:11 +0500

    refactor(dto): removed legacy DTOs

--- src/dto/test/section/create-section.dto.ts
+++
@@ -1,9 +0,0 @@
-import { SectionType } from '../../../config/enums/section-types.enum';
-
-export class CreateSectionDto {
-  testId: number;
-  sectionType: SectionType;
-  question: string;
-  options: string;
-  answer: string;
-}
--- src/dto/test/section/updated-section-data.dto.ts
+++
@@ -1,8 +0,0 @@
-import { SectionType } from '../../../config/enums/section-types.enum';
-
-export class UpdatedSectionDataDto {
-  sectionType?: SectionType;
-  question?: string;
-  option?: string;
-  answer?: string;
-}
